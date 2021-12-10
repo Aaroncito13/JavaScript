a8986b2fb4cf1ad20fbf1a61b7bcc45c16369a28
Aaron <93211523+Aaroncito13@users.noreply.github.com>
2021-12-10 13:50:25 +0100

    Add files via upload

---
+++ Tarefa 3/escripito.js
@@ -0,0 +1,23 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+/*as comillas cambian todo*/
+
+
+
+
+var moito = 234.3
+var dous = 190.2
+var tres = 90.12
+var cuatro = 210.9
+var raiz = Math.sqrt(moito);
+alert(raiz);
+var min = Math.min(moito, tres);
+alert(min);
+var cuadrado = Math.pow (cuatro, 2);
+alert(cuadrado);
+alert(Math.PI);
+alert(Math.random(0, 3));
