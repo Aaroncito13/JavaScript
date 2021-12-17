993000b78b321afb8d689f1e510f91d806e9e32c
Aaron <93211523+Aaroncito13@users.noreply.github.com>
2021-12-17 13:12:52 +0100

    Add files via upload

---
+++ Tarefa 6/Tarefa 1/escripito.js
@@ -0,0 +1,22 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+alert(24*2);/*as comillas cambian todo*/
+
+alert ("bos"+"dias"); /*Con una sola comilla tamén funciona*/
+
+alert ("👽")
+
+
+
+ var gastoHoxe;
+    gastoHoxe = 4646664;
+    alert(gastoHoxe);
+   
+   var gastoHoxe = 12.22;
+    var gastoOnte = 45.33;
+    var total = gastoHoxe + gastoOnte;
+    alert(total);
