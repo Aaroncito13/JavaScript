edf720c6c028a52f3420f885e8d593aecdbe0fcc
Aaron <93211523+Aaroncito13@users.noreply.github.com>
2021-12-17 13:26:38 +0100

    Add files via upload

---
+++ Tarefa_6/escripito.js
@@ -0,0 +1,31 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+/*as comillas cambian todo*/
+
+var desexo = confirm("Aceptas as cookis?");
+    if (desexo == true){
+    	alert("Grazas cookis aceptadas");
+    } else {
+    	alert("Cookis rechazadas :(");
+    }
+
+var posicion;
+    posicion = prompt("Cal e o teu posto?   ");
+    if(posicion == 1) {
+    	alert("medalla de oro");
+    }
+    if(posicion == 2) {
+    	alert("medalla de plata");
+    }
+    if(posicion == 3) {
+    	alert("medalla de bronce");
+    }
+  if(posicion > 4) {
+    	alert("Sen medalla grazas por participar");
+    }
+    
+    
